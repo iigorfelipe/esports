86418aeede639a9200cf341ae4036697a17dbd87
Igor <iigorfelipe@gmail.com>
2022-09-16 14:29:55 -0300

    🔧 config: add index.html in content and create backgroundImage

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,14 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')"
+      }
+    },
   },
   plugins: [],
 }
