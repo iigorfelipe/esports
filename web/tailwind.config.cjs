2c326af09c6b72df35aac73384545ab790956c98
Igor <iigorfelipe@gmail.com>
2022-09-16 19:39:46 -0300

    🎨 style: add fontFamily and news backgroundImage

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,9 +5,16 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
+      colors: {
+      },
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')"
+        galaxy: "url('/background-galaxy.png')",
+        nlwGradient: 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        gameGradient: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       }
     },
   },
