38ce5a4c7386b699bb48368ec00834aa059bc5bc
Igor <iigorfelipe@gmail.com>
2022-09-16 21:25:28 -0300

    ✨ feat: add logo

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
     </View>
   );
 }
