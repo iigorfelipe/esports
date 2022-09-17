e8c30ece85f3b2b9f15866d6f1f719c67abc9028
Igor <iigorfelipe@gmail.com>
2022-09-16 21:11:25 -0300

    ✨ feat: add fonts in App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,23 @@
 import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
 
 import { Background } from './src/components/Background';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    useFonts,
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
     <Background>
       <StatusBar
