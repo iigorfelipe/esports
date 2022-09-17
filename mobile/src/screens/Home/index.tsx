8bd011f9fa76e76787130e9ae891d55db459dd93
Igor <iigorfelipe@gmail.com>
2022-09-16 21:47:40 -0300

    ✨ feat: add Heading Component in Home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -3,15 +3,24 @@ import { View, Image } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 
+import { Heading } from '../../components/Heading';
+
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+
       <Image
         source={logoImg}
         style={styles.logo}
       />
+
+      <Heading
+        title='Encontre seu duo!'
+        subTitle='Selecione o game que deseja jogar...'
+      />
+
     </View>
   );
 }
