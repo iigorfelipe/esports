610c8ed7617dc124486718f07830650f713351ea
Igor <iigorfelipe@gmail.com>
2022-09-18 19:21:24 -0300

    ♻️ refactor: change button to Dialog.Trigger

--- web/src/components/AdBanner.tsx
+++ web/src/components/AdBanner.tsx
@@ -1,4 +1,5 @@
 import { MagnifyingGlassPlus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 const CreateAdBanner = () => {
   return (
@@ -17,10 +18,10 @@ const CreateAdBanner = () => {
 
         </div>
 
-        <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+        <Dialog.Trigger className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
           <MagnifyingGlassPlus size={24} />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
 
       </div>
     </div>
