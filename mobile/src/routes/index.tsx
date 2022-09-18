601d6ac5998b0b9fa2f5f95afd6f8194570c52b6
Igor <iigorfelipe@gmail.com>
2022-09-18 20:52:35 -0300

    ✨ feat: create routes

---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import {  NavigationContainer } from '@react-navigation/native';
+
+import AppRoutes from './app.routes';
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <AppRoutes />
+    </NavigationContainer>
+  )
+};
+
+export default Routes;
