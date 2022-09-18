e2b78e43b21827c37743e3a40ca06ffa9c4b90eb
Igor <iigorfelipe@gmail.com>
2022-09-18 15:59:36 -0300

    ♻️ refactor: componentize ad banner

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/main.css';
-import { MagnifyingGlassPlus } from 'phosphor-react';
 import logoImg from './assets/logo.svg';
 import GameBanner from './components/GameBanner';
+import CreateAdBanner from './components/AdBanner';
 
 const App = () => {
   return (
@@ -21,20 +21,8 @@ const App = () => {
         <GameBanner bannerUrl="/game-6.png" adsCount={4} title="World of WarCraft" />
       </div>
 
-      <div className='pt-1 bg-nlwGradient self-stretch rounded-lg mt-8 overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+      <CreateAdBanner />      
 
-          <div>
-            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-            <span className='text-zinc-400 block'>Publique seu anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
     </div>
   );
 };
