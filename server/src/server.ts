b0deead0c134f903934799fb44c16916bf693a6e
Igor <iigorfelipe@gmail.com>
2022-09-18 15:27:50 -0300

    ✨ feat: use cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 import * as H from './helpers';
  
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient({
   log: ['query']
