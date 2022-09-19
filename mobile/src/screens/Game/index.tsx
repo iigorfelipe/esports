58aa5123367fd9c25c1d52504ca5b226650aa88c
Igor <iigorfelipe@gmail.com>
2022-09-19 15:03:16 -0300

    ✨ feat: renders the games fetched from the backend

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,4 +1,5 @@
-import { Image, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { GameParams } from '../../@types/navigation';
@@ -9,11 +10,19 @@ import logoImg from '../../assets/logo-nlw-esports.png';
 import { THEME } from '../../theme';
 import { styles } from './styles';
 
-import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
-
+import { Background } from '../../components/Background';
+import { DuoCard, DuoCardProps } from  '../../components/DuoCard';
 
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+
+  useEffect(() => {
+    fetch(`http://192.168.0.99:3001/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => setDuos(data))
+  }, []);
+
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
@@ -55,6 +64,30 @@ export function Game() {
         <Heading
           title={game.title}
           subTitle='Conecte-se e comece a jogar!'
+        />
+
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={duos}
+          renderItem={({ item }) => (
+            <DuoCard
+              data={item}
+              onConect={() => {}}
+            />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[
+            duos.length > 0
+            ? styles.contentList
+            : styles.emptyListContent
+          ]}  
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
         />
 
       </SafeAreaView>
