4e81c41796fa87fa08dc4ab983bb137e8d804aa0
Igor <iigorfelipe@gmail.com>
2022-09-18 22:27:27 -0300

    ♻️ refactor: create GameParams interface

--- mobile/src/@types/navigation.d.ts
+++ mobile/src/@types/navigation.d.ts
@@ -1,12 +1,14 @@
+interface GameParams {
+  id: string;
+  title: string;
+  bannerUrl: string;
+}
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
-      game: {
-        id: string;
-        title: string;
-        bannerUrl: string;
-      };
+      game: GameParams;
     }
   }
 }
