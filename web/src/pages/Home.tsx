10728d589e5d6eae6123add45a786205d2b83c2b
Igor <iigorfelipe@gmail.com>
2022-09-23 19:15:12 -0300

    ♻️ refactor: add Games in Home page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,43 +1,22 @@
-import { useEffect, useState } from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from '../assets/logo.svg';
 
-import { Game } from '../types/Game';
-import getAllGames from '../api/getAllGames';
-
-import GameBanner from '../components/GameBanner';
 import CreateAdBanner from '../components/AdBanner';
 import CreateAdModal from '../components/AdModal';
-
+import Games from '../components/Games';
 
 const Home = () => {
-  const [games, setGames] = useState<Game[]>([]);
-
-  useEffect(() => {
-    (async () => setGames(await getAllGames()))();
-  }, []);
-
   return (
-    <div className='max-w-7xl mx-auto flex flex-col items-center my-9'>
+    <div className='max-w-[1366px] mx-auto flex flex-col items-center px-4 sm:px-8 md:px-10 my-10 mb-20'>
+
       <img src={logoImg} alt='' />
 
-      <h1 className='text-6xl text-white font-black mt-12'>
+      <h1 className='text-3xl font-black text-white lg:text-6xl mt-2'>
         Seu <span className='text-transparent bg-nlwGradient bg-clip-text'>duo</span> está aqui.
       </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-12'>
-        {
-          games.map(({ id, bannerUrl, title, _count: { ads } }) => (
-            <GameBanner
-              key={id}
-              bannerUrl={bannerUrl}
-              adsCount={ads}
-              title={title}
-            />
-          ))
-        }
-      </div>
+      <Games />
 
       <Dialog.Root>
 
@@ -45,7 +24,6 @@ const Home = () => {
         <CreateAdModal />        
 
       </Dialog.Root>
-
     </div>
   );
 };
