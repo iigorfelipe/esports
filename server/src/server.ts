dc0f9cb85f6e17fd2d66cc47c38fff238d30a744
Igor <iigorfelipe@gmail.com>
2022-09-22 22:09:47 -0300

    ✨ feat: add corsOptions

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,8 +5,14 @@ import * as H from './helpers';
  
 const app = express();
 
+const corsOptions ={
+  origin: '*', 
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const prisma = new PrismaClient({
   log: ['query']
