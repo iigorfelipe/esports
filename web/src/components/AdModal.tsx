b100c2bb1c3388803e990880fe37a67faec3c18b
Igor <iigorfelipe@gmail.com>
2022-09-24 16:05:35 -0300

    ♻️ refactor: change form spacing

--- web/src/components/AdModal.tsx
+++ web/src/components/AdModal.tsx
@@ -7,7 +7,7 @@ const CreateAdModal = () => {
 
       <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
 
-      <Dialog.Content className='fixed bg-[#2A2634] py-4 px-6 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+      <Dialog.Content className='fixed bg-[#2A2634] py-8 px-4 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
 
         <Dialog.Title className='text-3xl font-black'>
           Publique um anúncio
