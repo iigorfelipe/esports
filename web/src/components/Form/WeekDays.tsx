909e1b2b8600b9a73983271ff7db700b48027b59
Igor <iigorfelipe@gmail.com>
2022-09-24 21:23:05 -0300

    ♻️ refactor: render days of the week dynamically

--- web/src/components/Form/WeekDays.tsx
+++ web/src/components/Form/WeekDays.tsx
@@ -1,5 +1,6 @@
 import { SetStateAction } from 'react';
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import { days } from '../../helpers/weekDays';
 
 type Props = {
   weekDays: string[];
@@ -17,103 +18,25 @@ const WeekDays = ({ weekDays, setWeekDays }: Props) => {
         value={weekDays}
         onValueChange={setWeekDays}
       >
-        <ToggleGroup.Item
-          value='0'
-          title='Domingo'
-          className={
-            `w-6 h-6 rounded ${
-              weekDays.includes('0')
-                ? 'bg-violet-500'
-                : 'bg-zinc-900'
-            }`
-          }
-        >
-          D
-        </ToggleGroup.Item>
-        
-        <ToggleGroup.Item
-          value='1'
-          title='Segundo'
-          className={
-            `w-6 h-6 rounded ${
-              weekDays.includes('1')
-                ? 'bg-violet-500'
-                : 'bg-zinc-900'
-            }`
-          }
-        >
-          S
-        </ToggleGroup.Item>
-        
-        <ToggleGroup.Item
-          value='2'
-          title='Terça'
-          className={
-            `w-6 h-6 rounded ${
-              weekDays.includes('2')
-                ? 'bg-violet-500'
-                : 'bg-zinc-900'
-            }`
-          }
-        >
-          T
-        </ToggleGroup.Item>
-        
-        <ToggleGroup.Item
-          value='3'
-          title='Quarta'
-          className={
-            `w-6 h-6 rounded ${
-              weekDays.includes('3')
-                ? 'bg-violet-500'
-                : 'bg-zinc-900'
-            }`
-          }
-        >
-          Q
-        </ToggleGroup.Item>
-        
-        <ToggleGroup.Item
-          value='4'
-          title='Quinta'
-          className={
-            `w-6 h-6 rounded ${
-              weekDays.includes('4')
-                ? 'bg-violet-500'
-                : 'bg-zinc-900'
-            }`
-          }
-        >
-          Q
-        </ToggleGroup.Item>
-        
-        <ToggleGroup.Item
-          value='5'
-          title='Sexta'
-          className={
-            `w-6 h-6 rounded ${
-              weekDays.includes('5')
-                ? 'bg-violet-500'
-                : 'bg-zinc-900'
-            }`
-          }
-        >
-          S
-        </ToggleGroup.Item>
-        
-        <ToggleGroup.Item
-          value='6'
-          title='Sábado'
-          className={
-            `w-6 h-6 rounded ${
-              weekDays.includes('6')
-                ? 'bg-violet-500'
-                : 'bg-zinc-900'
-            }`
-          }
-        >
-          S
-        </ToggleGroup.Item>
+
+        {
+          days.map(({ value, title, text}) => (
+            <ToggleGroup.Item
+              value={value}
+              title={title}
+              className={
+                `w-10 h-6 rounded ${
+                  weekDays.includes(value)
+                    ? 'bg-violet-500'
+                    : 'bg-zinc-900'
+                }`
+              }
+            >
+              {text}
+            </ToggleGroup.Item>
+          ))
+        }
+
       </ToggleGroup.Root>
 
     </div>
